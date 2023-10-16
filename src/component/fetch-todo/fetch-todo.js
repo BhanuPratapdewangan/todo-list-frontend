2a956b17f4c7c14bfd231c768a8a67dc1d168fab
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-16 18:23:17 +0530

    modify : fetch

--- src/component/fetch-todo/fetch-todo.js
+++ src/component/fetch-todo/fetch-todo.js
@@ -12,15 +12,41 @@ const FetchTodo = () => {
     getData();
   }, [])
 
-  const getData = () => {
-    axios({
-      method:"get",
-      url: "https://todo-list-backend-eqk4.onrender.com/getdata",
-    }).then(response => {
-      setData(response.data);
-    })
-  }
+  // const getData = () => {
+  //   axios({
+  //     method: "get",
+  //     url: "https://todo-list-backend-eqk4.onrender.com/getdata",
+  //   }).then(response => {
+  //     setData(response.data);
+  //   })
+  // }
 
+
+  const getData = async() => {
+    let Data = await fetch("https://todo-list-backend-eqk4.onrender.com/getdata", {
+      method: "get",
+      body: JSON.stringify({ id, title, completed }),
+      headers: {
+        'Content-Type': 'application/json',
+        // authorization: `bearer ${JSON.parse(localStorage.getItem('token'))}`
+      }
+    })
+
+    Data = await Data.json();
+    if(Data) {
+      setData(Data);
+    }
+
+    // localStorage.setItem('token', JSON.stringify(data.auth));
+    // localStorage.setItem('user', JSON.stringify(data.data));
+
+    // if (data.auth) {
+    //   alert("Login Successfully...!");
+    //   Navigate("/home");
+    // } else {
+    //   alert("Incorrect input");
+    // }
+  }
 
   return (
     <div>
@@ -34,7 +60,7 @@ const FetchTodo = () => {
               <tr key={list.id}>
                 <td>{list.id}</td>
                 <td>{list.title}</td>
-                <td>{(list.completed) === true ? <input type='checkbox' className='form-check-input' disabled checked style={{backgroundColor:"green", color:"white"}} /> : <input type='checkbox' className='form-check-input' style={{ backgroundColor: "green", color: "white" }} />}</td>
+                <td>{(list.completed) === true ? <input type='checkbox' className='form-check-input' disabled checked style={{ backgroundColor: "green", color: "white" }} /> : <input type='checkbox' className='form-check-input' style={{ backgroundColor: "green", color: "white" }} />}</td>
                 <td><Link to=''><button className='btn btn-primary'><span className='bi bi-pen'></span></button></Link></td>
                 <td><button className='btn btn-danger'><span className='bi bi-trash'></span></button></td>
               </tr>
