12cc9ecab2a7134ad839e6e617984f5af1284e99
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-17 13:27:28 +0530

    fix : navigate

--- src/component/fetch-todo/fetch-todo.js
+++ src/component/fetch-todo/fetch-todo.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 const FetchTodo = () => {
 
